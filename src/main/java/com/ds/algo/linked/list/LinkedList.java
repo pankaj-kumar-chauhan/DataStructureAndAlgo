b6340cc84ceb3922a80906a5b7a405ddef26a420
pankaj-kumar-chauhan <pankaj9986649363@gmail.com>
2023-10-12 11:41:39 +0530

    Added link list constructor.

---
+++ src/main/java/com/ds/algo/linked/list/LinkedList.java
@@ -0,0 +1,40 @@
+package com.ds.algo.linked.list;
+
+public class LinkedList {
+
+	private Node head;
+	private Node tail;
+	private int length;
+
+	public LinkedList(int value) {
+		Node newNode = new Node(value);
+		head = newNode;
+		tail = newNode;
+		length = 1;
+	}
+
+	public Node getHead() {
+		return head;
+	}
+
+	public void setHead(Node head) {
+		this.head = head;
+	}
+
+	public Node getTail() {
+		return tail;
+	}
+
+	public void setTail(Node tail) {
+		this.tail = tail;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+}
